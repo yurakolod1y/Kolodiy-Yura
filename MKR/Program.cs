563f204381150e7a4e6b4a6107397f744d84969d
Bjolevid <130607948+Bjolevid@users.noreply.github.com>
2024-11-26 09:57:36 +0200

    First commit

---
+++ MKR/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.Write("What is your name? ");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine($"Nice to meet you, {userName}!");
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
